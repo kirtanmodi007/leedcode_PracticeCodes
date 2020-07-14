d6416e8c4edb8c85ba6eb9df26ed1a5d3110867b
kirtanmodi007 <kirtanmodi007@gmail.com>
2020-07-13 22:58:41 -0400

    Update Problem_1_TwoSum.java

--- Problem_1_TwoSum.java
+++ Problem_1_TwoSum.java
@@ -2,6 +2,35 @@ class Solution
 {
     public int[] twoSum(int[] nums, int target) 
     {
+        
+        //Optimized Solution
+        
+         HashMap<Integer, Integer> myHashMap = new HashMap<Integer, Integer>();
+        
+        int myArray[] = new int[2];
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            int difference = target - nums[i];
+            
+            if(myHashMap.containsKey(difference))
+            {
+                myArray[0] = myHashMap.get(difference);
+                myArray[1] = i;
+                return myArray;
+            }
+            
+            else
+            {
+                myHashMap.put(nums[i],i);
+            }
+        }
+        
+        return myArray;
+        
+        //Not opptimized Solution
+        
+        /*
         int[] indicesArray = new int[2];
         
         for(int i = 0; i <=nums.length-1; i++)
@@ -19,5 +48,6 @@ class Solution
         }
         
         return indicesArray;
+        */
 }
 }
